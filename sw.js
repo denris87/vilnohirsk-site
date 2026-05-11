31ae244ef58c6952d440254870c825dd32f69ca5
denris87 <denris87@gmail.com>
2026-05-11 17:49:01 +0200

    Update cache version numbers in sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'smart-vilnohirsk-static-v10.00.26';
-const DYNAMIC_CACHE = 'smart-vilnohirsk-dynamic-v10.00.26';
+const CACHE_NAME = 'smart-vilnohirsk-static-v10.00.27';
+const DYNAMIC_CACHE = 'smart-vilnohirsk-dynamic-v10.00.27';
 
 // Статические файлы, которые кешируются при первой загрузке (чтобы сайт открывался без сети)
 const STATIC_ASSETS = [
