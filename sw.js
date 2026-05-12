125a0f9416f42d430fa907c96784a718689097c5
denris87 <denris87@gmail.com>
2026-05-12 20:38:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'smart-vilnohirsk-static-v10.00.34';
-const DYNAMIC_CACHE = 'smart-vilnohirsk-dynamic-v10.00.34';
+const CACHE_NAME = 'smart-vilnohirsk-static-v10.00.35';
+const DYNAMIC_CACHE = 'smart-vilnohirsk-dynamic-v10.00.35';
 
 // Статические файлы, которые кешируются при первой загрузке (чтобы сайт открывался без сети)
 const STATIC_ASSETS = [
